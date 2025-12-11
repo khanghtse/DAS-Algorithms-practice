3bb87d7f0cd956bb78fa159dbc0c0d3e1948cc06
khanghtse <khanghoang090703@gmail.com>
2025-12-11 21:21:25 +0700

    linked-list Ex 1

---
+++ src/single_linked_list/Ex1.java
@@ -0,0 +1,20 @@
+package single_linked_list;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Ex1 {
+    // lam quen voi linked list
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            linkedList.offer(sc.nextInt());
+        }
+        for (int i : linkedList) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
